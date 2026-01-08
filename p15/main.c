6d2add5aa61d96906e07bb103f002f1864a1798a
Arnav <arnavkalekar27@gmail.com>
2026-01-08 17:29:01 -0500

    add!: cli

---
+++ p15/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+//#include <stdlib.h>
+//#include <math.h>
+
+int main(void) {
+
+    clock_t t0 = clock();
+
+
+
+    clock_t t1 = clock();
+
+
+
+    double cpu_time_used = ((double)(t1 - t0)) / CLOCKS_PER_SEC;
+    printf("Time: %f seconds\n", cpu_time_used);
+    return 0;
+
+}
