57d8f310331a1f48ebf6b5616cd2a6797b77d460
Arnav <arnavkalekar27@gmail.com>
2026-01-08 17:12:49 -0500

    add: incomplete soln p51

--- p51/p51.c
+++ p51/p51.c
@@ -4,12 +4,31 @@
 //#include <math.h>
 
 int main(void) {
-
-
-
     clock_t t0 = clock();
 
+    const int N = 999999;
+    const int size = (N - 1) / 2;
+    char is_prime[size];
+    for (int i = 0; i < size; i++) is_prime[i] = 1;
 
+    for (int i = 0; (2 * i + 3) * (2 * i + 3) <= N; i++) {
+        if (is_prime[i]) {
+            int p = 2 * i + 3;
+            int start = (p * p - 3) / 2;
+            for (int j = start; j < size; j += p) {
+                is_prime[j] = 0;
+            }
+        }
+    }
+
+    for (int digits = 1; digits <= 6; digits++) {
+        int start = (digits == 1) ? 1 : pow(10, digits - 1);
+        int end   = pow(10, digits) - 1;
+
+        for (int n = start; n <= end; n++) {
+            printf("%d\n", n);
+        }
+    }
 
     clock_t t1 = clock();
 
